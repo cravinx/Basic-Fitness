657e06eb475f61ae1ba1f36b65da3ac6b5e5c482
cravinx <unegbuprecious25@gmail.com>
2024-10-08 13:34:03 +0200

    Images

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const Gallery = () => {
 
     const gallery = [
-        "/public/images/img1.jpg",
-        "/public/images/img2.jpg",
-        "/public/images/img3.jpg",
-        "/public/images/img4.jpg",
-        "/public/images/img7.jpg",
-        "/public/images/img8.jpg",
-        "/public/images/img5.jpg",
-        "/public/images/img6.jpg",
+        "/images/img1.jpg",
+        "/images/img2.jpg",
+        "/images/img3.jpg",
+        "/images/img4.jpg",
+        "/images/img7.jpg",
+        "/images/img8.jpg",
+        "/images/img5.jpg",
+        "/images/img6.jpg",
     ];
 
   return (
